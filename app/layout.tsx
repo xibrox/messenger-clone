64a083318cabd9cc386a8bca369c3035bffd766e
xibrox <batonchik2004@gmail.com>
2023-07-18 21:14:58 +0200

    fixed icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+
       <body className={inter.className}>
         <AuthContext>
           <ToasterContext />
